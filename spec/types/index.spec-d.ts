0003a1b02e3deff9052b095109c1b10699c59293
uzlopak <aras.abbasi@googlemail.com>
2022-06-08 10:11:49 +0200

    add more tests

--- spec/types/index.spec-d.ts
+++ spec/types/index.spec-d.ts
@@ -1,7 +1,11 @@
-import { expectType } from 'tsd';
+import { expectError, expectType } from 'tsd';
 import { LicenseGenerator } from "../../types";
 
-expectType<string>(new LicenseGenerator({template: ''}).generate({data: {}}));
-expectType<Record<string | number, string>>(new LicenseGenerator<{[key: string]: any}>({template: ''}).parse({license: ''}).data);
-expectType<boolean>(new LicenseGenerator<{[key: string]: any}>({template: ''}).parse({license: ''}).valid);
-expectType<string>(new LicenseGenerator<{[key: string]: any}>({template: ''}).parse({license: ''}).serial);
+expectError(new LicenseGenerator());
+expectError(new LicenseGenerator({}));
+expectError(new LicenseGenerator({ serial: '' }));
+expectError(new LicenseGenerator({ template: 2 }));
+expectType<string>(new LicenseGenerator({ template: '' }).generate({ data: {} }));
+expectType<Record<string | number, string>>(new LicenseGenerator<{ [key: string]: any }>({ template: '' }).parse({ license: '' }).data);
+expectType<boolean>(new LicenseGenerator<{ [key: string]: any }>({ template: '' }).parse({ license: '' }).valid);
+expectType<string>(new LicenseGenerator<{ [key: string]: any }>({ template: '' }).parse({ license: '' }).serial);
