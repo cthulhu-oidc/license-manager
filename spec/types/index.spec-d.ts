6cc3fd41d5dac207b8198b17c77aecbb91c4a14a
uzlopak <aras.abbasi@googlemail.com>
2022-06-08 10:07:29 +0200

    extend typings tests

--- spec/types/index.spec-d.ts
+++ spec/types/index.spec-d.ts
@@ -2,3 +2,6 @@ import { expectType } from 'tsd';
 import { LicenseGenerator } from "../../types";
 
 expectType<string>(new LicenseGenerator({template: ''}).generate({data: {}}));
+expectType<Record<string | number, string>>(new LicenseGenerator<{[key: string]: any}>({template: ''}).parse({license: ''}).data);
+expectType<boolean>(new LicenseGenerator<{[key: string]: any}>({template: ''}).parse({license: ''}).valid);
+expectType<string>(new LicenseGenerator<{[key: string]: any}>({template: ''}).parse({license: ''}).serial);
